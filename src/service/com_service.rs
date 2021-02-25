55c64856d1781ddb1f92340f5963caa5207c82ac
万中胜 <wzs@ecwf.cn>
2021-02-25 14:01:33 +0800

    调整获取map sql

--- src/service/com_service.rs
+++ src/service/com_service.rs
@@ -11,7 +11,7 @@ use std::collections::HashMap;
 lazy_static! {
     static ref SQL_MAP:HashMap<String,String> = yml::read_sql("company");
 }
-fn load_str(key: &str) -> &str {
+fn load_sql(key: &str) -> &str {
     let sql = SQL_MAP.get(key);
     sql.unwrap().as_str()
 }
@@ -22,8 +22,8 @@ pub struct ComService {}
 
 impl ComService {
     pub async fn exist_name(&self, name: &String) -> Result<u64> {
-        let exist_name_sql = SQL_MAP.get("exist_name");
-        BASE_DB.py_fetch("", exist_name_sql.unwrap().as_str(), &json!({"name": name })).await
+        let exist_name_sql = load_sql("exist_name");
+        BASE_DB.py_fetch("", exist_name_sql, &json!({"name": name })).await
     }
     pub async fn get(&self, id: &String) -> Result<Company> {
         BASE_DB.fetch_by_id("", id).await
@@ -36,12 +36,12 @@ impl ComService {
         BASE_DB.save("", arg).await
     }
     pub async fn update(&self, arg: &Company) -> Result<DBExecResult> {
-        let update_sql = SQL_MAP.get("update");
-        BASE_DB.py_exec("", update_sql.unwrap().as_str(), arg).await
+        let update_sql = load_sql("update");
+        BASE_DB.py_exec("", update_sql, arg).await
     }
 
     pub async fn list(&self, name: &str, page_index: u64, page_size: u64) -> Result<Page<Company>> {
-        let list_sql = load_str("list");
+        let list_sql = load_sql("list");
         let param = &json!({"name": name });
         BASE_DB.py_fetch_page("", list_sql, param, &PageRequest::new(page_index, page_size)).await
     }
